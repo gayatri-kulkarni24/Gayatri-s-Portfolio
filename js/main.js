9618a0d90ef7590cccf89b73af6bdab4c0f96434
gayatri-kulkarni24 <gaytrikulkarni616@gmail.com>
2026-07-22 20:32:21 +0530

    Premium Navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("Portfolio Started Successfully");
+const menuBtn=document.querySelector(".menu-btn");
+
+const navLinks=document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click",()=>{
+
+navLinks.classList.toggle("active");
+
+});
+
+const navbar=document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>80){
+
+navbar.style.padding="0";
+
+navbar.style.background="rgba(5,8,22,.9)";
+
+}
+
+else{
+
+navbar.style.background="rgba(5,8,22,.6)";
+
+}
+
+});

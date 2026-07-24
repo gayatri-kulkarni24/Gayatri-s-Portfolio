ef2088750589af3c52352e85aeecfd187bb0fb5f
gayatri-kulkarni24 <gaytrikulkarni616@gmail.com>
2026-07-24 20:40:18 +0530

    Complete skills section

--- js/animation.js
+++ js/animation.js
@@ -1,102 +1,101 @@
+
+gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline();
 
-    tl.from(".logo", {
-        x: -50,
-        opacity: 0,
-        duration: 1
-    })
+tl.from(".logo", {
+  x: -50,
+  opacity: 0,
+  duration: 1,
+})
 
-    .from(".nav-links li", {
-        y: -40,
-        opacity: 0,
-        stagger: .12,
-        duration: .8
-    }, "-=0.5")
+  .from(
+    ".nav-links li",
+    {
+      y: -40,
+      opacity: 0,
+      stagger: 0.12,
+      duration: 0.8,
+    },
+    "-=0.5",
+  )
 
-    .from(".resume-btn", {
-        x: 40,
-        opacity: 0,
-        duration: .8
-    }, "-=0.5")
+  .from(
+    ".resume-btn",
+    {
+      x: 40,
+      opacity: 0,
+      duration: 0.8,
+    },
+    "-=0.5",
+  )
 
-    .from(".hero-animate", {
-        y: 60,
-        opacity: 1,
-        stagger: .18,
-        duration: 1
-    }, "-=0.3")
+  .from(
+    ".hero-animate",
+    {
+      y: 60,
+      opacity: 1,
+      stagger: 0.18,
+      duration: 1,
+    },
+    "-=0.3",
+  )
 
-    .from(".hero-image", {
-        scale: .8,
-        opacity: 1,
-        duration: 1
-    }, "-=0.7");
+  .from(
+    ".hero-image",
+    {
+      scale: 0.8,
+      opacity: 1,
+      duration: 1,
+    },
+    "-=0.7",
+  );
 
-
-    gsap.from(".tech-badge", {
-        scale: .5,
-        opacity: 1,
-        stagger: .15,
-        delay: 1.8,
-        duration: .8,
-        ease: "back.out(1.7)"
-    });
-    
-    gsap.to(".profile-circle", {
-        y: -15,
-        repeat: -1,
-        yoyo: true,
-        duration: 3,
-        ease: "sine.inOut"
-    });
-
-gsap.from(".section-title",{
-
-scrollTrigger:".about",
-
-y:60,
-
-opacity:0,
-
-duration:1
-
+gsap.from(".tech-badge", {
+  scale: 0.5,
+  opacity: 1,
+  stagger: 0.15,
+  delay: 1.8,
+  duration: 0.8,
+  ease: "back.out(1.7)",
 });
 
-gsap.from(".about-image",{
-
-scrollTrigger:".about",
-
-x:-100,
-
-opacity:0,
-
-duration:1
-
+gsap.to(".profile-circle", {
+  y: -15,
+  repeat: -1,
+  yoyo: true,
+  duration: 3,
+  ease: "sine.inOut",
 });
 
-gsap.from(".about-content",{
+gsap.from(".section-title", {
+  scrollTrigger: ".about",
 
-scrollTrigger:".about",
+  y: 60,
 
-x:100,
+  opacity: 0,
 
-opacity:0,
-
-duration:1
-
+  duration: 1,
 });
 
-gsap.from(".info-card",{
 
-scrollTrigger:".about",
+gsap.from(".about-content", {
+  scrollTrigger: ".about",
 
-y:40,
+  x: 100,
 
-opacity:0,
+  opacity: 0,
 
-stagger:.2,
-
-duration:.7
-
+  duration: 1,
 });
-gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".info-card", {
+  scrollTrigger: ".about",
+
+  y: 40,
+
+  opacity: 0,
+
+  stagger: 0.2,
+
+  duration: 0.7,
+});

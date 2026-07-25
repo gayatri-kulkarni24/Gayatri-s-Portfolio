06358f04f54a0c04f7c7c4e2a47786d14159bc30
gayatri-kulkarni24 <gaytrikulkarni616@gmail.com>
2026-07-25 20:48:50 +0530

    home stats added

--- js/stats.js
+++ js/stats.js
@@ -3,49 +3,27 @@ const counters = document.querySelectorAll(".counter");
 const counterObserver = new IntersectionObserver((entries) => {
 
     entries.forEach(entry => {
-
         if (!entry.isIntersecting) return;
-
         const counter = entry.target;
-
         const target = +counter.dataset.target;
-
         let count = 0;
-
         const speed = target / 60;
-
         const update = () => {
-
             count += speed;
-
             if (count < target) {
-
                 counter.innerText = Math.ceil(count);
-
                 requestAnimationFrame(update);
-
             } else {
-
                 counter.innerText = target;
-
             }
-
         };
-
         update();
-
-        counterObserver.unobserve(counter);
-
+      counterObserver.unobserve(counter);
     });
-
 }, {
-
-    threshold:0.5
-
+    threshold: 0.5
 });
 
 counters.forEach(counter => {
-
     counterObserver.observe(counter);
-
 });
